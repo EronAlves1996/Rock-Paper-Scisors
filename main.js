35e749cde16575778fc5576bef2272835df4f274
Eron Alves <eron.alves@rocketmail.com>
2022-03-06 15:52:04 -0300

    Criar função roundPlay

--- main.js
+++ main.js
@@ -2,3 +2,33 @@ function computerPlay(){
    return ["rock", "paper", "scissor"][Math.floor(Math.random() * 3)]
 }
 
+function roundPlay(playerSelection, computerSelection){
+   playerSelection = playerSelection.toLowerCase();
+   if (playerSelection === computerSelection){
+      return 0
+   }else{
+      switch(playerSelection){
+         case 'rock':
+            if(computerSelection === 'paper'){
+               return -1
+            }
+            else if(computerSelection === 'scissor'){
+               return 1
+            }
+         case 'paper':
+            if(computerSelection === 'rock'){
+               return 1
+            }
+            else if(computerSelection === 'scissor'){
+               return -1
+            }
+         case 'scissor': 
+            if(computerSelection === 'paper'){
+               return 1
+            }
+            else if(computerSelection === 'rock'){
+               return -1
+            }
+      }
+   }
+}
