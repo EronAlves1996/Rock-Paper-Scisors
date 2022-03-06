51afccc7867ff9a905caa6b2d7de63a432c851b7
Eron Alves <eron.alves@rocketmail.com>
2022-03-06 16:01:25 -0300

    Criar a função game

--- main.js
+++ main.js
@@ -32,3 +32,28 @@ function roundPlay(playerSelection, computerSelection){
       }
    }
 }
+
+function game(){
+   for(let i=0;i<5;i++){
+      alert("Welcome to Rock-Paper-Scissors!! \n This game will be played in 5 rounds")
+      let playerSelection = prompt("Insert here your guess:");
+      let computerSelection = computerPlay();
+      let points = [/*0 for player, 1 for computer*/];
+      let resuĺt = roundPlay(playerSelection, computerSelection);
+      switch (result){
+         case 1:
+            console.log(`You win!! Computer choosed ${computerSelection} and ${playerSelection} beats ${computerSelection}!`);
+            points[0]++;
+         case 0:
+            console.log("It's a tie!! You two choosed ${playerSelection}");
+         case -1:
+            console.log(`You loose!! Computer choosed ${computerSelection} and ${playerSelection} loose for ${computerSelection}!`);
+            points[1]++;
+      }
+   }
+   if(points[0]>points[1]){
+      console.log("You win the war!!!!");
+   } else{
+      console.log("You lose!");
+   }
+}
