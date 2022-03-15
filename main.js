b0a96f84bf94c2fe33b1c4b9d5128edb08732cf9
Eron Alves <eron.alves@rocketmail.com>
2022-03-15 00:30:55 -0300

    Refatoração, criação de restart

--- main.js
+++ main.js
@@ -35,83 +35,112 @@ function roundPlay(playerSelection, computerSelection){
    }
 }
 
-function game(){
-
-  /* Lógica do placar */
-
-  const score = document.createElement("div")
-
-  let points = [0/*0 for player*/,
-      /*1 for computer*/0];
-
-  function refreshScore(){
-    score.textContent=`Computer = ${points[1]}\nPlayer = ${points[0]}`;
-  }
-
-  document.body.appendChild(score);
-
-  refreshScore();
-  /*Lógica dos botões */
-  const buttons = [], display = document.createElement("div");
-
-  for(let i = 0;i<3;i++){
-    buttons.push(document.createElement("button"));
-    switch(i){
-      case 0:
-        buttons[i].id = "rock";
-        break;
-      case 1:
-        buttons[i].id = "paper"
-        break;
-      case 2:
-        buttons[i].id = "scissor"
-        break;
-    }
-  }
-
-  let playerSelection = "", computerSelection = "";
-
-  buttons.forEach(n => {
-    n.onclick = e => {
-      computerSelection = computerPlay();
-      playerSelection = e.target.id;
-      changeDisplay(roundPlay(playerSelection, computerSelection))
-    };
-    document.body.appendChild(n);
-  });
-
-  /* Lógica do display */
-
-  document.body.appendChild(display);
-  
-  function changeDisplay(winvalue){
-    switch (winvalue){
-         case 1:
-            display.textContent = `You win!! Computer choosed ${computerSelection} and ${playerSelection} beats ${computerSelection}!`;
-            points[0]++;
-            break;
-         case 0:
-            display.textContent = `It's a tie!! You two choosed ${playerSelection}`;
-            break;
-         case -1:
-            display.textContent = `You loose!! Computer choosed ${computerSelection} and ${playerSelection} loose for ${computerSelection}!`;
-            points[1]++;
-            break;
-    }
-    points.forEach(n => {
-      if(n == 5){
-        buttons.forEach(n => n.onclick = undefined);
-      }
-    })
-    if (points[0] == 5) display.textContent = "You win!!"
-    else if(points[1] == 5) display.textContent = "You loose!!"
-    refreshScore();
-  }
-
-  /*   
-   if(points[0]>points[1]){
-      return "You win the war!!!!"
-   } else{
-      return "You lose!"
-   }*/
+function score(computerPoints, playerPoints){
+  this.scoreDisplay = document.createElement("div");
+  this.computerPoints = computerPoints;
+  this.playerPoints = playerPoints;
+  this.refreshScore = function () {
+    this.scoreDisplay.textContent=`Computer = ${this.computerPoints}\nPlayer = ${this.playerPoints}`;
+  };
 }
+
+function display(){
+  this.resultDisplay = document.createElement("div");
+  this.changeDisplay = function(winvalue, computerSelection, playerSelection, displayScore){
+    switch (winvalue){
+      case 1:
+        this.resultDisplay.textContent = `You win!! Computer choosed ${computerSelection} and ${playerSelection} beats ${computerSelection}!`;
+        displayScore.playerPoints++;
+        break;
+      case 0:
+        this.resultDisplay.textContent = `It's a tie!! You two choosed ${playerSelection}`;
+        break;
+      case -1:
+        this.resultDisplay.textContent = `You loose!! Computer choosed ${computerSelection} and ${playerSelection} loose for ${computerSelection}!`;
+        displayScore.computerPoints++;
+        break;
+    }
+    displayScore.refreshScore();
+  }
+}
+
+function checkWin(displayScore, display, buttons){
+  if (displayScore.playerPoints == 5) {
+    display.resultDisplay.textContent = "You win!!";
+    buttons.deactivateButtons(display, displayScore);
+    offerRestart();
+  } else if (displayScore.computerPoints == 5) {
+    display.resultDisplay.textContent = "You loose!!";
+    buttons.deactivateButtons(display, displayScore);
+    offerRestart();
+  }
+}
+
+function restartGame(){
+  document.body.innerHTML = "";
+  game();
+}
+
+function offerRestart(){
+  let restartButton = document.createElement("button");
+  restartButton.textContent = "Restart";
+  restartButton.addEventListener("click", restartGame);
+  document.body.appendChild(restartButton);
+}
+
+function doPlaying(e, displayScore, display, buttons){
+  let computerSelection = computerPlay();
+  console.log(e.target.id);
+  let playerSelection = e.target.id;
+  display.changeDisplay(roundPlay(playerSelection, computerSelection), computerSelection, playerSelection, displayScore);
+  checkWin(displayScore, display, buttons);
+}
+
+function button(){
+  this.playingButtons = document.createElement("div");
+  this.createButtons = function(){
+    this.playingButtons.classList.add = "buttons";
+    for(let i = 0;i<3;i++){
+      let tempButton = document.createElement("button");
+      switch(i){
+        case 0:
+          tempButton.id = "rock";
+          break;
+        case 1:
+          tempButton.id = "paper"
+          break;
+        case 2:
+          tempButton.id = "scissor"
+          break;
+      }
+      this.playingButtons.appendChild(tempButton);
+  }};
+  this.activateButtons = function(display, score){
+    this.playingButtons.addEventListener('click', f = (e) => { doPlaying(e, score, display, this) });
+  };
+  this.deactivateButtons = function(){
+    this.playingButtons.removeEventListener('click', f, false);
+  };
+}
+
+function game(){  
+
+  /* Criar Placar, botões e display de resultados em memória */
+
+  const displayScore = new score(0,0);    
+  const buttons = new button();
+  const endDisplay = new display();
+
+  /* Inicializar elementos criados anteriormente */
+
+  displayScore.refreshScore();
+  buttons.createButtons();
+  buttons.activateButtons(endDisplay, displayScore);
+
+  /* Atrelar elementos ao DOM */
+
+  document.body.appendChild(displayScore.scoreDisplay);
+  document.body.appendChild(buttons.playingButtons);  
+  document.body.appendChild(endDisplay.resultDisplay);
+}
+
